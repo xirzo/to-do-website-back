cd6525aa69914152a89c54a730c3e7d619877583
EastTemplar <EastTem@gmail.com>
2024-06-29 17:08:58 +0300

    feat: documentation

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { AutoIncrement, Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface UserCreationAttribute {
@@ -7,10 +8,15 @@ interface UserCreationAttribute {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttribute> {
+  @ApiProperty({ example: '1', description: 'Уникальный идентификатор пользователя' })
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number;
+
+  @ApiProperty({ example: 'user@gmail.com', description: 'Уникальный адрес почты пользователя' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
+
+  @ApiProperty({ example: 'qwerty', description: 'Пароль пользователя' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 }
