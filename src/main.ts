4c2c53dc19c6a077cac0a442949474ae239c56e2
EastTemplar <EastTem@gmail.com>
2024-06-29 14:06:30 +0300

    learn: Basic concepts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
+
 bootstrap();
